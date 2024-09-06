42e89b1688092b26f216c2fcd5c345d867956d8b
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-09-06 13:21:59 +0100

    final renaming script

--- scripts/file_renaming/rename.sh
+++ scripts/file_renaming/rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script renames all files in a directory to a new name by adding a prefix to the file name.
-# It takes the directory path and prefix as arguments. 
+# It takes the directory path and prefix as arguments.
 # The script defaults to the current directory if no argument is passed.
 
 dir=${1:-.}
@@ -9,13 +9,16 @@ prefix=$2
 
 for file in $dir/*; do
     if [ -f "$file" ]; then
-    newfile="{$prefix}_$file"
+        # Extract the file name and create a new name with the prefix and the current year
+        newfile="$prefix_(basename "$file")_$(date +%Y)"
         mv "$file" "$dir/$newfile"
-    fi
 
-    if [ $? -eq 0 ]; then
-        echo "File $file renamed to $newfile successfully"
-    else
-        echo "An error occurred while renaming $file"
+ # Check if the rename was successful and give feedback
+        if [ $? -eq 0 ]; then
+            echo "File $file renamed to $newfile successfully"
+        else
+            echo "An error occurred while renaming $file"
+        fi
+
     fi
 done
