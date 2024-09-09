67a03669ff7aa453caad077457a0c73e35827385
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-09-09 10:15:40 +0100

    modified script to list lines of error

--- scripts/backup/log_analysis.sh
+++ scripts/backup/log_analysis.sh
@@ -7,12 +7,22 @@ if [ ! -f "$LOG_FILE" ]; then
     exit 1
 fi
 
-
 function search_keyword() {
-    local keywords=$@
+    local keywords=$1
     echo "Searching for '$keywords' in the logs..."
     count=$(grep -i "$keywords" "$LOG_FILE" | wc -l)
     echo "Found $count occurences of '$keywords'"
+
+    # Check if any occurrences were found
+    if [ "$count" -gt 0 ]; then
+        echo "Listing the lines containing '$keywords:'"
+        grep -i "$keywords" "$LOG_FILE"
+        echo "________________________"
+    else
+        echo "No occurences of '$keywords' found."
+    fi
+
 }
 
-search_keyword "error" "warning"
+search_keyword "error"
+search_keyword "warning"
