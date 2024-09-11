041f8367cb481b03aa7bd62e1c725745a276fdfb
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-09-11 09:05:43 +0100

    archive script

---
+++ scripts/archive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dir=$1
+
+if [ ! -d $dir ]; then
+ echo "$dir is not a valid directory."
+exit 1
+fi
+
+if [ -z $dir ]; then
+echo "No directory provided. Usage: $0 <directory>"
+exit 1
+fi
+
+if [ -z $(ls -A $dir) ]; then
+echo "$dir is empty or does not contain any file."
+exit 1
+fi
+
+timestamp=$(date +"%Y%m%d_%H%M%S")
+archive_name="$(basename $dir)_$timestamp.tar.gz"
+
+tar -czf "$archive_name" -C "$dir" .
+echo "Backup of $dir completed!"
